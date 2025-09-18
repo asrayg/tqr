92856b5aedc0e87d386c0a2d96d81f01d00793bc
Asray Gopa <69517250+asrayg@users.noreply.github.com>
2025-09-17 23:05:36 -0500

    Cleaning up positioning code. Added timing patern logic for QR code to have the 3 positioning squares

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,52 @@
 fn main() {
-    println!("Hello, world!");
     let mut arr: [[bool; 21]; 21] = [[false; 21]; 21];
     arr = addPositioningThings(arr);
-    // println!("{:?}",arr);
-    for i in arr.iter(){
-        for x in i.iter(){
-                print!("{}", if x == &true {"\u{2588}\u{2588}"} else {"  "} );
+    arr = addTimingPatterns(arr);
+    for i in arr.iter() {
+        for x in i.iter() {
+            print!("{}", if x == &true { "\u{2588}\u{2588}" } else { "  " });
         }
         println!("")
     }
 }
 
-fn addPositioningThings(mut arr: [[bool; 21]; 21]) -> [[bool; 21]; 21]{
+fn addPositioningThings(mut arr: [[bool; 21]; 21]) -> [[bool; 21]; 21] {
     arr = placePositioning(arr, 0, 0);
-    arr = placePositioning(arr, arr.len()-7, 0);    
-    arr = placePositioning(arr, 0, arr.len()-7);
+    arr = placePositioning(arr, arr.len() - 7, 0);
+    arr = placePositioning(arr, 0, arr.len() - 7);
+    arr
+}
 
+fn addTimingPatterns(mut arr: [[bool; 21]; 21]) -> [[bool; 21]; 21] {
+    for col in 8..13 {
+        arr[6][col] = col % 2 == 0;
+    }
+
+    for row in 8..13 {
+        arr[row][6] = row % 2 == 0;
+    }
 
     arr
 }
 
-fn placePositioning(mut arr: [[bool; 21]; 21],offsetX :usize,offsetY :usize) -> [[bool; 21]; 21]{
-    //outside top
-    arr[0+offsetX][0+offsetY] = true;
-    arr[1+offsetX][0+offsetY] = true;
-    arr[2+offsetX][0+offsetY] = true;
-    arr[3+offsetX][0+offsetY] = true;
-    arr[4+offsetX][0+offsetY] = true;
-    arr[5+offsetX][0+offsetY] = true;
-    arr[6+offsetX][0+offsetY] = true;
-
-    //outside  right
-    arr[6+offsetX][0+offsetY] = true;
-    arr[6+offsetX][1+offsetY] = true;
-    arr[6+offsetX][2+offsetY] = true;
-    arr[6+offsetX][3+offsetY] = true;
-    arr[6+offsetX][4+offsetY] = true;
-    arr[6+offsetX][5+offsetY] = true;
-    arr[6+offsetX][6+offsetY] = true;
-
-    //outside  left
-    arr[0+offsetX][0+offsetY] = true;
-    arr[0+offsetX][1+offsetY] = true;
-    arr[0+offsetX][2+offsetY] = true;
-    arr[0+offsetX][3+offsetY] = true;
-    arr[0+offsetX][4+offsetY] = true;
-    arr[0+offsetX][5+offsetY] = true;
-    arr[0+offsetX][6+offsetY] = true;
-
-    //outside bottom
-    arr[0+offsetX][6+offsetY] = true;
-    arr[1+offsetX][6+offsetY] = true;
-    arr[2+offsetX][6+offsetY] = true;
-    arr[3+offsetX][6+offsetY] = true;
-    arr[4+offsetX][6+offsetY] = true;
-    arr[5+offsetX][6+offsetY] = true;
-    arr[6+offsetX][6+offsetY] = true;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //inside top
-    arr[2+offsetX][2+offsetY] = true;
-    arr[3+offsetX][2+offsetY] = true;
-    arr[4+offsetX][2+offsetY] = true;  
-
-    //inside  right
-    arr[4+offsetX][2+offsetY] = true;
-    arr[4+offsetX][3+offsetY] = true;
-    arr[4+offsetX][4+offsetY] = true;
-
-    //inside  left
-    arr[2+offsetX][2+offsetY] = true;
-    arr[2+offsetX][3+offsetY] = true;
-    arr[2+offsetX][4+offsetY] = true;
-
-    //inside bottom
-    arr[2+offsetX][4+offsetY] = true;
-    arr[3+offsetX][4+offsetY] = true;
-    arr[4+offsetX][4+offsetY] = true;
-
-    //inside middle
-    arr[3+offsetX][3+offsetY] = true;
-
-
+fn placePositioning(
+    mut arr: [[bool; 21]; 21],
+    offset_x: usize,
+    offset_y: usize,
+) -> [[bool; 21]; 21] {
+    for i in 0..7 {
+        for j in 0..7 {
+            if i == 0
+                || i == 6
+                || j == 0
+                || j == 6
+                || ((i == 2 || i == 4) && (j >= 2 && j <= 4))
+                || ((j == 2 || j == 4) && (i >= 2 && i <= 4))
+                || (i == 3 && j == 3)
+            {
+                arr[offset_x + i][offset_y + j] = true;
+            }
+        }
+    }
     arr
-
-
 }
